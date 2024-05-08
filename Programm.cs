2434c7c050bea5aef9915a8b192f2bfc9f382efe
Katerinka58 <144324135+Katerinka58@users.noreply.github.com>
2024-05-08 10:35:33 +0300

    Update Programm.cs

--- Programm.cs
+++ Programm.cs
@@ -1,40 +1,89 @@
 using System;
-using System.Collections.Generic;
 
-public class User
+namespace ConsoleApp
 {
-    public string Login { get; set; }
-    public string Password { get; set; }
-}
-
-public class UserArray<T> : GenericArray<T> where T : User
-{
-    public UserArray(int size) : base(size) { }
-
-    public void RegisterUser(string login, string password)
+    internal class Program
     {
-        T user = (T)Activator.CreateInstance(typeof(T));
-        user.Login = login;
-        user.Password = password;
-        Add(user);
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        UserArray<User> userArray = new UserArray<User>(3);
-
-        userArray.RegisterUser("user1", "password1");
-        userArray.RegisterUser("user2", "password2");
-        userArray.RegisterUser("user3", "password3");
-
-        Console.WriteLine("Пользователи:");
-        for (int i = 0; i < userArray.Length; i++)
+        static void Main(string[] args)
         {
-            User user = userArray.Get(i);
-            Console.WriteLine($"Логин: {user.Login}, Пароль: {user.Password}");
+            Registration<string, int> user = new Registration<string, int>(5);
+            user.UserRegistration("Игорь", 123456);
+            user.UserRegistration("Никита2017", 18052017);
+
+            for (int i = 0; i < user.Length(); i++)
+            {
+                DataBase<string, int> database = user.Get((uint)i);
+                Console.WriteLine("Username: " + database.Username + ", Password: " + database.Password);
+            }
+        }
+    }
+
+    public class ArrayG<T>
+    {
+        private int size;
+        private T[] array;
+
+        public ArrayG(uint size)
+        {
+            array = new T[size];
+            this.size = 0;
+        }
+        public void Add(T item)
+        {
+            if (size < array.Length)
+            {
+                array[size] = item;
+                size++;
+            }
+            else
+                Console.WriteLine("Массив переполнен!");
+        }
+        public void Remove(uint index)
+        {
+            if (index < size)
+            {
+                array[index] = default(T);
+            }
+            else
+                Console.WriteLine("Некорректный индекс!");
+        }
+        public T Get(uint index)
+        {
+            if (index < size)
+                return array[index];
+            else
+                return default(T);
+        }
+        public string Get()
+        {
+            string str = "";
+            return str = string.Join(", ", array);
+        }
+        public int Length()
+        {
+            return size;
+        }
+    }
+
+    public class DataBase<TUsername, TPassword>
+    {
+        public TUsername Username { get; private set; }
+        public TPassword Password { get; private set; }
+        public DataBase(TUsername username, TPassword password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+    public class Registration<TUsername, TPassword> : ArrayG<DataBase<TUsername, TPassword>>
+    {
+        public Registration(uint size) : base(size)
+        {
+        }
+        public void UserRegistration(TUsername username, TPassword password)
+        {
+            DataBase<TUsername, TPassword> user = new DataBase<TUsername, TPassword>(username, password);
+            Add(user);
         }
     }
 }
